b7199bc924442669c3eb829600bf3652ba9d6463
Maheerr2707 <62205613+Maheerr2707@users.noreply.github.com>
2021-06-22 23:30:26 +0530

    Add files via upload

--- datavisualization.py
+++ datavisualization.py
@@ -2,8 +2,9 @@ import pandas as pd
 import plotly.express as px
 
 
-df=pd.read_csv("Covid data.csv")
+df=pd.read_csv("Covid Data.csv")
 print(df)
 
-fig = px.line(df,x="date",y="cases",color="country",title="Covid cases of countries")
+fig = px.scatter(df,x="date",y="cases",color="country", title="Covid cases of countries")
+
 fig.show()
